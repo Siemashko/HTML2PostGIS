fce91805d2f20f9f9efb9167418a41c1cb689deb
siemashko <k.siemaszko@student.mini.pw.edu.pl>
2019-11-24 17:47:29 +0100

    HTML mini-project 3

--- rest-project/src/main/java/com/siembrzoz/rest/controller/dto/CreatePackageRequestDto.java
+++ rest-project/src/main/java/com/siembrzoz/rest/controller/dto/CreatePackageRequestDto.java
@@ -1,6 +1,5 @@
 package com.siembrzoz.rest.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
