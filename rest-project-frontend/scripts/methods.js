f2eb6961fc8c7169e49e14fdc1a948abd0c73d84
lukaszbrzozowski <lukaszbrzozowski1998@gmail.com>
2019-11-24 21:07:21 +0100

    Update methods.js

--- rest-project-frontend/scripts/methods.js
+++ rest-project-frontend/scripts/methods.js
@@ -258,6 +258,16 @@ async function initializeApplication() {
     document.getElementById("package-list").innerHTML="";
     var listOfDeliveryPackages = await findAll();
     listOfDeliveryPackages.forEach(package => addPackageToList(package));
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [50 *0.707, 82*0.707],
+        iconAnchor: [25*0.707, 82*0.707],
+        popupAnchor: [1, -34],
+        shadowSize: [82*0.707, 82*0.707]
+    });
+    L.marker([52.2, 21], {icon: greenIcon}).addTo(markerLayerGroup);
+
 }
 
 function addPackageToList(package) {
